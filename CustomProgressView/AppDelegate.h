8e791273223e69c2f019a2395ea721efd1e307d8
lemo <lemo@lemodeiMac.local>
2018-05-12 14:08:45 +0800

    Initial Commit

---
+++ CustomProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomProgressView
+//
+//  Created by lemo on 2018/5/12.
+//  Copyright © 2018年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomProgressView
+//
+//  Created by lemo on 2018/5/12.
+//  Copyright © 2018年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
